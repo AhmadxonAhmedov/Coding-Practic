9a3d962736dca8977d114bcc8d2cc56545e57fd0
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-05-15 21:30:56 +0500

    Добавьте файлы проекта.

---
+++ Coding Practic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Coding_Practic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
