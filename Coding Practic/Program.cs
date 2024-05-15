eb7d25f1ea55cad188f9b8b0b5675b55e2f30913
Azam Amonov <azamamonov555@gmail.com>
2024-05-15 21:38:41 +0500

    N/A

--- Coding Practic/Program.cs
+++ Coding Practic/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // text ichindagi belgilarni sonini aniqlash
+            
+            string text = "123ertyuERT4567$%^&*";
+            int numbers = 0;
+            int lowerLetters = 0;
+            int upperLetters = 0;
+            int others = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ( text is null)
+                {
+                    
+                }
+            }
+            
         }
     }
 }
