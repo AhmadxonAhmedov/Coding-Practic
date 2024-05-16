d57929a8fa4ad10cf7eaaa2d8a5d6714fc296057
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-05-16 23:53:15 +0500

    Add classes

--- Coding Practic/Program.cs
+++ Coding Practic/Program.cs
@@ -1,25 +1,14 @@
-﻿namespace Coding_Practic
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Coding_Practic
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // text ichindagi belgilarni sonini aniqlash
-
-            string text = "123ertyuERT4567$%^&*";
-            int numbers = 0;
-            int lowerLetters = 0;
-            int upperLetters = 0;
-            int others = 0;
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (text[i] <= '1' && text[i] >= '9')
-                {
-                    numbers++;
-                }
-            }
-            Console.WriteLine(numbers);
-        }
+            IfOperator ifOperator = new IfOperator();
+            ifOperator.InfoOperator();
+        }   
     }
 }
