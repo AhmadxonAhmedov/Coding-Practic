1092104430406e36bf2b3e0c6b7b2294d678971e
Azam Amonov <azamamonov555@gmail.com>
2024-05-16 22:21:13 +0500

    DATA: Add Solutions

---
+++ Coding Practic/Solutions/ISolution.Array.Easy.cs
@@ -0,0 +1,10 @@
+namespace Coding_Practic.Solutions;
+
+public partial interface ISolution
+{
+    void FindMax(int[] numbers);
+    void FindMin(int[] numbers);
+    void FindMinAndMax(int[] numbers);
+    void FindMaxAndMinDiff(int[] numbers);
+    void ReverseArray(int[] numbers);
+}
---
+++ Coding Practic/Solutions/Solution.Esay.Array.cs
@@ -0,0 +1,32 @@
+namespace Coding_Practic.Solutions;
+
+public partial class Solution
+{
+    public void FindMax(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FindMin(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FindMinAndMax(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FindMaxAndMinDiff(int[] numbers)
+    {
+        // Given an array of integers,
+        // return the difference between the
+        // largest and smallest integers in the array .
+        throw new NotImplementedException();
+    }
+
+    public void ReverseArray(int[] numbers)
+    {
+        throw new NotImplementedException();
+    }
+}
